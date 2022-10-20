239c2db4699c166650c9cc21b2f8fecc135dd8c8
Oliver <111798851+oliver-keyspace@users.noreply.github.com>
2022-10-20 16:56:01 -0400

    Fix overflow

--- Sources/CLVM/Casts.swift
+++ Sources/CLVM/Casts.swift
@@ -126,9 +126,9 @@ public extension SignedInteger {
         for i in 0..<ndigits {
             var thisdigit: UInt8 = self_bytes[i]
             if do_twos_comp {
-                thisdigit = (thisdigit ^ MASK) + carry
-                carry = thisdigit >> SHIFT
-                thisdigit &= MASK
+                let thisdigit16 = UInt16(thisdigit ^ MASK) + UInt16(carry)
+                carry = UInt8(thisdigit16 >> SHIFT)
+                thisdigit = UInt8(thisdigit16 & UInt16(MASK))
             }
             
             /* Because we're going LSB to MSB, thisdigit is more
